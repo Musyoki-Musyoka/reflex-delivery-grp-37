0480e53baa98a628d81dd3ede991782a1bc8616e
annemutahi <ms.nyamburamutahi@gmail.com>
2026-09-04 06:01:38 +0300

    made changes

--- reflex/settings.py
+++ reflex/settings.py
@@ -20,8 +20,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',') if os.getenv('ALLOWED_HOSTS') else ["127.0.0.1", "localhost"]
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',') if os.getenv('ALLOWED_HOSTS') else ["127.0.0.1", "localhost"]
+
 
 # Application definition
 
